faa62804dc193a0c83f25c8dcf242d4b6813b079
haein <fkdl1223@naver.com>
2020-06-08 09:45:19 +0900

    Step10_AbstractClass added

--- Step10_AbstractClass/src/test/main/MainClass01.java
+++ Step10_AbstractClass/src/test/main/MainClass01.java
@@ -16,7 +16,7 @@ package test.main;
 import test.mypac.MyWeapon;
 import test.mypac.Weapon;
 
-public class MainClasss01 {
+public class MainClass01 {
 	public static void main(String[] args) {
 		//Weapon type 의 참조값을 얻어내는게 목적!
 		Weapon w1=new MyWeapon();
